2a8ba6e2ff826186431b513d7e846c0fc5dc4aa9
tahaela <128037097+tahaela@users.noreply.github.com>
2023-03-17 14:12:08 +0100

    Update VerificateurMotDePasse.java

--- VerificateurMotDePasse.java
+++ VerificateurMotDePasse.java
@@ -28,10 +28,49 @@ public class VerificateurMotDePasse {
         }
         
         // Vérifie qu'il y a au moins une lettre majuscule, une lettre minuscule et un chiffre
-        if (!aMajuscule || !aMinuscule || !aChiffre ) {
+        if (!aMajuscule || !aMinuscule || !aChiffre package Bibliotheque;
+
+public class VerificateurMotDePasse {
+
+    public static boolean estValide(String motDePasse) {
+
+    	// Vérifie la longueur
+        if (motDePasse.length() < 8 && motDePasse.length() > 15) {
+            return false;
+        }
+        
+        boolean aMajuscule = false;
+        boolean aMinuscule = false;
+        boolean aChiffre = false;
+        boolean aCarcSpec = false;
+        
+        
+        // Vérifie chaque caractère
+        for (int i = 0; i < motDePasse.length(); i++) {
+            char c = motDePasse.charAt(i);
+            if (Character.isUpperCase(c)) {
+                aMajuscule = true;
+            } else if (Character.isLowerCase(c)) {
+                aMinuscule = true;
+            } else if (Character.isDigit(c)) {
+                aChiffre = true;
+            } else if (isCaracSpec(c)) {
+                aCarcSpec = true;
+            }
+                
+       }
+        
+        // Vérifie qu'il y a au moins une lettre majuscule, une lettre minuscule et un chiffre
+        if (!aMajuscule || !aMinuscule || !aChiffre || !aCarcSpec) {
             return false;
         }
         
         return true;
+    }
+
+    public static boolean isCaracSpec(char cr) {
+        // Vérifie si le caractère est un caractère spécial
+        // Retourne true s'il est spécial, false
+        return false;
     }
 }
