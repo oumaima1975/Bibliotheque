868a53c76a2393d73b2f440927ae2948d1aa9d5f
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2023-03-17 13:53:31 +0100

    Add files via upload

---
+++ VerificateurMotDePasse.java
@@ -0,0 +1,41 @@
+public class VerificateurMotDePasse {
+
+    public static boolean estValide(String motDePasse) {
+
+    	// Vérifie la longueur
+        if (motDePasse.length() < 8 && motDePasse.length() > 15) {
+            return false;
+        }
+        
+        boolean aMajuscule = false;
+        boolean aMinuscule = false;
+        boolean aChiffre = false;
+        boolean aCarcSpec = false;
+        
+        
+        // Vérifie chaque caractère
+        for (int i = 0; i < motDePasse.length(); i++) {
+            char c = motDePasse.charAt(i);
+            if (Character.isUpperCase(c)) {
+                aMajuscule = true;
+            } else if (Character.isLowerCase(c)) {
+                aMinuscule = true;
+            } else if (Character.isDigit(c)) {
+                aChiffre = true;
+            } else if (isCaracSpec(c)) {
+            	aCarcSpec = true;
+            }
+        }
+        
+        public boolean isCaracSpec(char cr) {
+        	
+        }
+        
+        // Vérifie qu'il y a au moins une lettre majuscule, une lettre minuscule et un chiffre
+        if (!aMajuscule || !aMinuscule || !aChiffre || aCarcSpec) {
+            return false;
+        }
+        
+        return true;
+    }
+}
